43eff9c3fb228262cec90e104b7564e31318cfa2
@MusaWiga2022 <musalordkai@gmail.com>
2023-04-25 09:00:31 -0700

    done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,18 @@
-// import React from 'react'
+import React from 'react'
 
 
-// const Home = (props) => {
-//   return (
-//     <div>
-//             {props.showAddTask && <AddTask onAdd={props.addTask} />}
-//               {props.tasks.length > 0 ?(
-//                 <Tasks tasks={tasks}
-//                   onDelete={deleteTask}
-//                   toggleReminder={toggleReminder} 
-//                   />
-//    ) : ("No tasks to show")}
-//     </div>
-//   )
-// }
+const Home = (props) => {
+  return (
+    <div>
+            {props.showAddTask && <AddTask onAdd={props.addTask} />}
+              {props.tasks.length > 0 ?(
+                <Tasks tasks={tasks}
+                  onDelete={deleteTask}
+                  toggleReminder={toggleReminder} 
+                  />
+   ) : ("No tasks to show")}
+    </div>
+  )
+}
 
-// export default Home
+export default Home
